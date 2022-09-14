59563b8d10d9cbe232f94b2af3d660eb76a9d69a
Caleb Horton <c.alex.horton@gmail.com>
2022-09-14 14:21:56 -0500

    Added 'Delete' method

--- routes/entries.js
+++ routes/entries.js
@@ -4,6 +4,7 @@ const authorization = require('../middleware/authorization')
 
 router.post('/', authorization, async (req, res) => {
     try {
+        console.log("Submition", req);
         const { content, emotion, flagged, notes } = req.body;
         const entry = await pool.query(
             "INSERT INTO article (content, emotion, flagged, notes, user_id) VALUES ($1, $2, $3, $4, $5)", [content, emotion, flagged, notes, req.user])
@@ -34,6 +35,19 @@ router.get('/:id', authorization, async (req, res) => {
     } catch (err) {
         console.log(err.message);
         res.status(500).json('Server Error');
+    }
+})
+
+router.delete('/', authorization, async (req, res) => {
+    try {
+        console.log('Id to be deleted: ', req.body)
+
+        const delEntry = await pool.query('DELETE FROM article WHERE article_id = $1', [req.body.id])
+        console.log(delEntry);
+        res.status(202).json('Entry Deleted!')
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).json('Server error while deleting')
     }
 })
 

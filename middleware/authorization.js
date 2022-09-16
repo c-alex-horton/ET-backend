cd3726037b8c0ecbbdf7fcfe236fdf245ff76ef2
Caleb Horton <c.alex.horton@gmail.com>
2022-09-16 14:47:05 -0500

    Renamed error on failed JWT

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 
 module.exports = (req, res, next) => {
     try {
-        //.1 Destructure token
+        //Destructure token
         const jwtToken = req.header('token');
 
         if (!jwtToken) {
-            return res.status(403).json("Not Authorized - JWT section")
+            return res.status(403).json("No Token")
         }
 
         const payload = jwt.verify(jwtToken, process.env.JWT_SECRET)
@@ -17,6 +17,6 @@ module.exports = (req, res, next) => {
         next();
     } catch (err) {
         console.error(err.message);
-        return res.status(503).json("Not Authorized")
+        return res.status(503).json("Invalid Token")
     }
 }
--- utils/jwtGenerator.js
+++ utils/jwtGenerator.js
@@ -5,7 +5,7 @@ const jwtGenerator = (user_id) => {
     const payload = {
         user: user_id
     }
-    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "2hr" })
+    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "15min" })
 }
 
 module.exports = jwtGenerator;

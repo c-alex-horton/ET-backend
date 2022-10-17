8798a2c690b5fe68a7fa522994af0a4e249b5a0e
Caleb Horton <c.alex.horton@gmail.com>
2022-10-17 17:10:26 -0500

    Clear for v2

--- redis.js
+++
@@ -1,9 +0,0 @@
-const Redis = require('redis')
-
-const redis_client = Redis.createClient();
-
-redis_client.on('error', err => console.log(err));
-
-redis_client.connect()
-
-module.exports = redis_client;
